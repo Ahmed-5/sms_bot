6c1f53ea1de17e7f3b1c9cfa142909eafebaaf7f
Ahmed-5 <ahmedelhassen11@gmail.com>
2021-11-29 21:19:50 +0200

    solved +249 and 0 numbers issue

--- lib/repo/telegram.dart
+++ lib/repo/telegram.dart
@@ -1,8 +1,21 @@
-class Telegram{
+class Telegram {
   String body;
   String address;
   bool isCurrentUser;
   int unixDate;
 
-  Telegram({this.body, this.address, this.isCurrentUser, this.unixDate});
+  Telegram({this.body, this.address, this.isCurrentUser, this.unixDate}) {
+    this.address = processAddress(this.address);
+  }
+
+  String processAddress(String address) {
+    if (address.startsWith("0")) {
+      return "+249" + address.substring(1);
+    }
+    if (address.startsWith("249")) {
+      return "+" + address;
+    } else {
+      return address;
+    }
+  }
 }
